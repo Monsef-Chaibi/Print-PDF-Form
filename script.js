40c8a25ec2cf9acb7d516e3c0a6155746eb52f11
Monsef-Chaibi <xr.monsef@gmail.com>
2024-01-25 13:39:49 +0100

    v12

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= 20; i++) {
         document.getElementById('in' + i).addEventListener('input', function() {
             document.getElementById('out' + i).textContent = this.value;
         });
     }
 });
+function printDiv() {
+    var divContents = document.getElementById("print").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<body>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
